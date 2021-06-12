c4f64127b2d9b48ca41815a9083488abd7ad265a
Victor Marri <victor.marri@dtidigital.com.br>
2021-06-12 14:51:29 -0300

    Models, Data, Context adicionados

---
+++ GraphQL/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Speaker> Speakers { get; }
+    }
+}
